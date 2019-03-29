e8f52a8b17ef2cab13e4b78e2749f277a79236ef
Mistral Contrastin <me@madgen.net>
2019-03-29 16:30:07 +0000

    Use Exalog's range restriction chercker

--- src/Language/Vanillalog/Stage.hs
+++ src/Language/Vanillalog/Stage.hs
@@ -15,6 +15,7 @@ import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Language.Exalog.Core as E
 import qualified Language.Exalog.Relation as R
 import qualified Language.Exalog.Logger as Log
+import           Language.Exalog.RangeRestriction (checkRangeRestriction)
 
 import           Language.Vanillalog.AST
 import qualified Language.Vanillalog.Generic.AST as AG
@@ -22,7 +23,6 @@ import           Language.Vanillalog.Generic.Compiler (compile)
 import qualified Language.Vanillalog.Parser.Lexer as Lexer
 import qualified Language.Vanillalog.Parser.Parser as Parser
 import qualified Language.Vanillalog.Generic.Parser.Lexeme as L
-import           Language.Vanillalog.Generic.RangeRestriction (checkRangeRestriction)
 import           Language.Vanillalog.Generic.Transformation.Query (nameQueries)
 import           Language.Vanillalog.Transformation.Normaliser (normalise)
 
@@ -34,17 +34,14 @@ lex = Lexer.lex
 parse :: Stage Program
 parse = Parser.programParser
 
-rangeRestricted :: Stage Program
-rangeRestricted file bs = do
-  ast <- parse file bs
-  checkRangeRestriction ast
-  pure ast
-
 namedQueries :: Stage Program
-namedQueries file = rangeRestricted file >=> nameQueries
+namedQueries file = parse file >=> nameQueries
 
 normalised :: Stage Program
 normalised file = namedQueries file >=> normalise
 
 compiled :: Stage (E.Program 'E.ABase, R.Solution 'E.ABase)
-compiled file = normalised file >=> compile
+compiled file bs = do
+  res@(pr, _) <- (normalised file >=> compile) bs
+  checkRangeRestriction pr
+  pure res
